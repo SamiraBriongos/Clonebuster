fb93cca316b2873b14d324384d1c17d410de7e6b
SamiraBriongos <sambriongos@gmail.com>
2023-08-28 23:56:32 +0200

    Update prepare_attack_samples.sh

--- source/Evaluation/prepare_attack_samples.sh
+++ source/Evaluation/prepare_attack_samples.sh
@@ -7,7 +7,9 @@ dataends=$4
 
 
 echo "reading from " $filein "and writing in" $fileout
-head -n $dataends $filein | tail -n $datastarts > temp_file
+s=$(($dataends-$datastarts))
+echo $s
+head -n $dataends $filein | tail -n $s > temp_file
 wc temp_file
-cat $filein | grep Tim | awk '{print $2","$3","$4","$5","$6","$7","$8","$9","$10","$11","$12","$13","$14","$15","$16","$17","$19","$20","$21","$22","$23","$24","$25","$26","$27","$28","$29","$30","$31","$32","$33","$34","$36",1";}' >> $fileout
+cat temp_file | grep Tim | awk '{print $2","$3","$4","$5","$6","$7","$8","$9","$10","$11","$12","$13","$14","$15","$16","$17","$19","$20","$21","$22","$23","$24","$25","$26","$27","$28","$29","$30","$31","$32","$33","$34","$36",1";}' >> $fileout
 rm temp_file
